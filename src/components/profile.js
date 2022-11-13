2854c0e2f7ec55e55f242d592077e6bb72ddd7a6
LAPTOP-1MG4FIS2\msyah <mochamadsahrul28@gmail.com>
2022-11-14 00:57:24 +0700

    sesi 1

---
+++ src/components/profile.js
@@ -0,0 +1,15 @@
+import React from "react"
+
+class Profile extends React.Component{
+    render(){
+        return(
+            <div>
+                <h1>
+                    Sesi 1
+                </h1>
+            </div>
+        )
+    }
+}
+
+export default Profile
